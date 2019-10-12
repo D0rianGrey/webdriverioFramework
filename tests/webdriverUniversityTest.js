10f9f0d675a32bb99a0892ff265da01de831da6f
eugene <utyfdfrthby08@gmail.com>
2019-10-12 23:17:04 +0300

    push from PC (webstorm#2)

--- tests/webdriverUniversityTest.js
+++ tests/webdriverUniversityTest.js
@@ -1,42 +1,43 @@
+
 describe("Verify whether webdriveruniversity link on homepage work correctly", function () {
     it("check that the contact us button opens the contact us page", function (done) {
         browser.setViewportSize({
             width: 1200,
             height: 800
-        })
+        });
         browser.url('/');
-        var title = browser.getTitle()
+        let title = browser.getTitle();
         expect(title).to.equal('WebDriverUniversity.com');
         console.log('Title is: ' + title);
 
-        browser.click('#contact-us')
-        var tabIds = browser.getTabIds();
+        browser.click('#contact-us');
+        let tabIds = browser.getTabIds();
         console.log(tabIds);
         browser.switchTab(tabIds[1]);
 
-        var title2 = browser.getTitle()
+        let title2 = browser.getTitle();
         expect(title2).to.equal('WebDriver | Contact Us');
 
-        var url = browser.getUrl();
+        let url = browser.getUrl();
         expect(url).to.include('Contact-Us', 'URL Mismatch');
         browser.close();
     });
 
     it("check that the login button opens the login portal page", function (done) {
-        browser.url('/')
-        var title = browser.getTitle();
+        browser.url('/');
+        let title = browser.getTitle();
         expect(title).to.equal('WebDriverUniversity.com');
         title.should.equal('WebDriverUniversity.com');
         console.log('Title is: ' + title);
 
-        browser.click('#login-portal')
-        var tabIds = browser.getTabIds();
+        browser.click('#login-portal');
+        let tabIds = browser.getTabIds();
         browser.switchTab(tabIds[1]);
 
-        var title2 = browser.getTitle()
+        let title2 = browser.getTitle();
         expect(title2).to.equal('WebDriver | Login Portal');
 
-        var url = browser.getUrl();
+        let url = browser.getUrl();
         expect(url).to.include('Login-Portal', 'URL Mismatch');
 
     });
